11a8f1f8ae5e2123f47209aedb09c035b6ef0bcf
Nicolas <nicolasgabriel321@gmail.com>
2025-10-03 19:07:50 -0300

    ultima versão do programa gerador de senhas

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,14 +14,17 @@ function generatePassword() {
     const numberChars = "0123456789";
     const symbolChars = "!@#$%^&*()_+[]{}|;:,.<>?";
 
-    if (upperCaseCheckEl.checked) { 
-        chars += upperCaseChars; }
+    if (upperCaseCheckEl.checked) {
+        chars += upperCaseChars;
+    }
 
-    if (numberCheckEl.checked) { 
-        chars += numberChars; }
+    if (numberCheckEl.checked) {
+        chars += numberChars;
+    }
 
-    if (symbolCheckEl.checked) { 
-        chars += symbolChars; }
+    if (symbolCheckEl.checked) {
+        chars += symbolChars;
+    }
 
     let password = "";
 
@@ -32,20 +35,69 @@ function generatePassword() {
 
 
     inputEl.value = password;
+
     calculateQuality()
+    calculateFontSize()
 
 }
 
 function calculateQuality() {
-    // 20% -> crítico -> 100% safe
-    // 64/64 = 1 -> 100%
+    // T*0.25 + M*0.15 + N*0.25 + S*0.35 = 100%
+    // Tamanho, Maiusculas, Numeros, Simbolos
 
-    const percent = Math.round((passwordLength / 64) * 100)
+    const percent = Math.round(
+        (passwordLength / 64) * 25 +
+        (upperCaseCheckEl.checked ? 15 : 0)) +
+        (numberCheckEl.checked ? 25 : 0) +
+        (symbolCheckEl.checked ? 35 : 0);
 
     console.log(percent)
 
     securityIndicatorBarEl.style.width = `${percent}%`;
-    
+
+    if (percent > 69) {
+        // safe
+        securityIndicatorBarEl.classList.remove("critical");
+        securityIndicatorBarEl.classList.remove("warning");
+        securityIndicatorBarEl.classList.add("safe");
+    } else if (percent > 50) {
+        // warning
+        securityIndicatorBarEl.classList.remove("critical");
+        securityIndicatorBarEl.classList.add("warning");
+        securityIndicatorBarEl.classList.remove("safe");
+    } else {
+        // critical
+        securityIndicatorBarEl.classList.add("critical");
+        securityIndicatorBarEl.classList.remove("warning");
+        securityIndicatorBarEl.classList.remove("safe");
+    }
+
+    if (percent >= 100) {
+        securityIndicatorBarEl.classList.add("completed");
+    } else {
+        securityIndicatorBarEl.classList.remove("completed");
+    }
+
+}
+
+function calculateFontSize() {
+    if (passwordLength > 45) {
+        inputEl.classList.remove("font-sm");
+        inputEl.classList.remove("font-xs");
+        inputEl.classList.add("font-xxs");
+    } else if (passwordLength > 32) {
+        inputEl.classList.remove("font-sm");
+        inputEl.classList.add("font-xs");
+        inputEl.classList.remove("font-xxs");
+    } else if (passwordLength > 22) {
+        inputEl.classList.add("font-sm");
+        inputEl.classList.remove("font-xs");
+        inputEl.classList.remove("font-xxs");
+    } else {
+        inputEl.classList.remove("font-sm");
+        inputEl.classList.remove("font-xs");
+        inputEl.classList.remove("font-xxs");
+    }
 }
 
 function copyPassword() {
@@ -65,6 +117,7 @@ symbolCheckEl.addEventListener("click", generatePassword);
 
 document.querySelector("#copy-1").addEventListener("click", copyPassword);
 document.querySelector("#copy-2").addEventListener("click", copyPassword);
+document.querySelector("#renew").addEventListener("click", generatePassword);
 
 function alert() {
     Swal.fire({
